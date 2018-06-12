42dc143336389ad95efa620d504fab3f59c36d7a
Yang Guo <gy@fir.im>
2018-06-12 13:53:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-toolbar
+      color="grey"
+      dark
+      fixed
+      app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data () {
+      return {
+        drawer: false
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/FlowList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" fixed app>
+      <v-list dense>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FlowList',
+    props: {
+      drawer: Boolean
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>

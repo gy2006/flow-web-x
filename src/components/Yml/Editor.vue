aa85e201a49858f525a23460401d6b9269fcce7b
egunyoa <yang.a.guo@ericsson.com>
2019-05-03 14:06:55 +0200

    fix webpack config

--- src/components/Yml/Editor.vue
+++ src/components/Yml/Editor.vue
@@ -1,32 +1,21 @@
 <template>
   <div style="height: 100%; width: 100%; background: #1e1e1e;">
-    <div class="text-xs-center loading"  v-if="!editor">
+    <div class="text-xs-center loading" v-if="!editor">
       <v-progress-circular
-      indeterminate
-      color="purple"
+          indeterminate
+          color="purple"
       ></v-progress-circular>
     </div>
-    <MonacoEditor
-      v-if="editor"
-      language='yaml'
-      :code='editor'
-      theme='vs-dark'
-      :options='options'
-      :changeThrottle='500'
-      @mounted='onMounted'
-      @codeChange='onCodeChange'
-      ref='vscode'>
-    </MonacoEditor>
+    <div id="editor">
+    </div>
   </div>
 </template>
 
 <script>
-  import MonacoEditor from 'vue-monaco-editor'
+  // import * as monaco from 'monaco-editor'
+
   export default {
     name: 'Yml',
-    components: {
-      MonacoEditor
-    },
     data () {
       return {
         options: {
@@ -54,9 +43,13 @@
         default: ''
       }
     },
+    // mounted: function() {
+    //   monaco.editor.create(document.getElementById('editor'), {
+    //     value: 'console.log("Hello, world")',
+    //     language: 'javascript'
+    //   });
+    // },
     methods: {
-      onMounted (editor) {
-      },
       // 代码发生变化时触发
       onCodeChange (editor) {
         this.$emit('onCodeChange', editor.getValue())
@@ -67,12 +60,14 @@
 
 <style scoped>
   #language_options {
-      padding: 16px;
-	}
-	#language_options select {
-      height: 33px;
-      width: 20%;
-	}
+    padding: 16px;
+  }
+
+  #language_options select {
+    height: 33px;
+    width: 20%;
+  }
+
   .loading {
     height: 100%;
     width: 100%;
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+      }
+    },
+    plugins: [
+      new MonacoWebpackPlugin({
+        languages: ['javascript', 'css', 'html']
+      })
+    ]
+  },
+
+  chainWebpack: config => {
+    config.resolve.alias
+        .set('monaco-editor', 'monaco-editor/esm/vs/editor/editor.api.js');
+  },
+}

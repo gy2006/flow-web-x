ca7251d415178686630ccf1abb723f818cf8e8b4
gy2006 <benqyang.2006.se@gmail.com>
2019-05-17 15:09:24 +0200

    remove monaco vue plugin

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,10 @@
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
-
 module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
         'vue$': 'vue/dist/vue.esm.js'
       }
-    },
-    plugins: [
-      new MonacoWebpackPlugin()
-    ]
+    }
   },
 
   chainWebpack: config => {

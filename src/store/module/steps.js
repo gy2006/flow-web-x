82034bf42f951befade83ebec95795b185620ccf
gy2006 <benqyang.2006.se@gmail.com>
2019-06-04 16:47:14 +0200

    release object url after click

--- src/store/module/steps.js
+++ src/store/module/steps.js
@@ -77,14 +77,13 @@ const actions = {
   downloadLog ({commit}, cmdId) {
     let url = 'jobs/logs/' + cmdId + '/download'
     http.get(url, (response, file) => {
-      console.log(file)
-
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement('a');
       link.href = url;
       link.setAttribute('download', file);
       document.body.appendChild(link);
       link.click();
+      window.URL.revokeObjectURL(url);
     })
   }
 }

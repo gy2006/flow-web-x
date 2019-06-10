9f988cbd6c8f9eb949af94286e9bba90fefba88c
gy2006 <32008001@qq.com>
2019-06-10 23:23:17 +0200

    fix yml color on monaco plugin

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,15 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
 module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
         'vue$': 'vue/dist/vue.esm.js'
       }
-    }
-  },
+    },
 
-  chainWebpack: config => {
-    config.resolve.alias
-        .set('monaco-editor', 'monaco-editor/esm/vs/editor/editor.api.js');
-  },
+    plugins: [
+      new MonacoWebpackPlugin()
+    ]
+  }
 }

c91716abf79592133b3a416aab2777b426f97de3
gy2006 <benqyang.2006.se@gmail.com>
2019-08-12 16:59:13 +0200

    make text box for input on settings

---
+++ src/components/Common/TextBox.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="subheading font-weight-medium my-1">{{ title }}</div>
+    <v-text-field
+        solo
+        :readonly="readonly"
+        v-model="data"
+        :rules="rules"
+        class="text-box"
+    ></v-text-field>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TextBox',
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      data: {
+        type: String,
+        default: ''
+      },
+      readonly: {
+        type: Boolean,
+        required: false,
+        default: false
+      },
+      rules: {
+        type: Array,
+        default () {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .text-box {
+    .v-input__control {
+      min-height: 42px !important;
+    }
+    .v-input__slot {
+      background: #fafbfc !important;
+      font-size: 14px !important;
+    }
+  }
+</style>
--- src/view/Settings/Profile.vue
+++ src/view/Settings/Profile.vue
@@ -3,14 +3,36 @@
     <v-card-title class="pb-0 bottom-border">
       <v-breadcrumbs :items="navs" divider=">"></v-breadcrumbs>
     </v-card-title>
+
     <v-card-text class="pt-0">
+      <v-layout row wrap>
+        <v-flex xs6>
+          <text-box title="E-Mail"
+                    :data="user.email"
+                    readonly
+          ></text-box>
+          <text-box title="Role"
+                    :data="user.role"
+                    readonly
+          ></text-box>
+        </v-flex>
+
+        <v-flex xs5 class="mt-2 ml-4">
+          <div class="subheading font-weight-medium">Profile picture here</div>
+        </v-flex>
+      </v-layout>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+  import TextBox from '@/components/Common/TextBox'
+
   export default {
     name: 'SettingsProfile',
+    components: {
+      TextBox
+    },
     data () {
       return {
         navs: [ {

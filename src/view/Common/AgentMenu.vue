5737473090961f3d3da1c4cbd67bf0221a2dca32
gy2006 <32008001@qq.com>
2019-09-30 15:41:33 +0200

    adjust agent list layout

--- src/view/Common/AgentMenu.vue
+++ src/view/Common/AgentMenu.vue
@@ -11,18 +11,15 @@
                    :key="agent.id"
                    @click="onAgentItemClick"
       >
-        <div :class="[agent.color, 'agent-state']"></div>
-
-        <v-list-tile-avatar>
+        <div :class="[agent.color, 'state']"></div>
+        <div class="icon">
           <v-icon small>{{ agent.icon }}</v-icon>
-        </v-list-tile-avatar>
-
-        <v-list-tile-content>
+        </div>
+        <div class="title">
           <v-list-tile-title>{{ agent.name }}</v-list-tile-title>
-        </v-list-tile-content>
-
+        </div>
         <v-list-tile-action>
-<!--          <v-chip small light :color="agent.color" text-color="white">{{ $t(agent.text) }}</v-chip>-->
+          <!--          <v-chip small light :color="agent.color" text-color="white">{{ $t(agent.text) }}</v-chip>-->
         </v-list-tile-action>
       </v-list-tile>
     </v-list>
@@ -30,59 +27,75 @@
 </template>
 
 <script>
-  import actions from '@/store/actions'
-  import { subscribeTopic } from '@/store/subscribe'
-  import { util, AgentWrapper } from '@/util/agents'
-  import { mapState } from 'vuex'
+    import actions from '@/store/actions'
+    import {subscribeTopic} from '@/store/subscribe'
+    import {AgentWrapper, util} from '@/util/agents'
+    import {mapState} from 'vuex'
 
-  export default {
-    name: 'AgentMenu',
-    data () {
-      return {
-        items: []
-      }
-    },
-    mounted () {
-      this.$store.dispatch(actions.agents.list).then()
-      subscribeTopic.agents(this.$store)
-    },
-    computed: {
-      ...mapState({
-        agents: state => state.agents.items,
-        updated: state => state.agents.updated
-      })
-    },
-    watch: {
-      agents (after) {
-        this.items = util.convert(after)
-      },
+    export default {
+        name: 'AgentMenu',
+        data() {
+            return {
+                items: []
+            }
+        },
+        mounted() {
+            this.$store.dispatch(actions.agents.list).then()
+            subscribeTopic.agents(this.$store)
+        },
+        computed: {
+            ...mapState({
+                agents: state => state.agents.items,
+                updated: state => state.agents.updated
+            })
+        },
+        watch: {
+            agents(after) {
+                this.items = util.convert(after)
+            },
 
-      updated (after) {
-        let wrapper = new AgentWrapper(after)
-        this.$notify({
-          group: 'appInfo',
-          text: `Agent '${wrapper.name}' ${this.$t(wrapper.text)}`,
-          duration: 5000
-        })
-      }
-    },
-    methods: {
-      onAgentItemClick () {
-        // do nothing
-      }
+            updated(after) {
+                let wrapper = new AgentWrapper(after)
+                this.$notify({
+                    group: 'appInfo',
+                    text: `Agent '${wrapper.name}' ${this.$t(wrapper.text)}`,
+                    duration: 5000
+                })
+            }
+        },
+        methods: {
+            onAgentItemClick() {
+                // do nothing
+            }
+        }
     }
-  }
 </script>
 
 <style lang="scss">
   .agent-list {
-    .agent-state {
+    min-width: 200px;
+
+    .state {
       height: 100%;
       width: 5%;
     }
 
+    .icon {
+      margin-left: 5px;
+      margin-right: 5px;
+      width: 10%;
+    }
+
+    .title {
+      width: 50%;
+    }
+
     .v-list__tile {
       padding: 0 !important;
+    }
+
+    .agent-name {
+      min-width: 100px !important;
     }
   }
 </style>

19e3528d8e16c8e1bbabce343925f150c0dccf80
gy2006 <32008001@qq.com>
2019-11-04 17:04:07 +0100

    init plugin component

---
+++ src/components/Flow/Plugins.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="full-size">
+    <v-row>
+      <v-col>
+        Plugins
+      </v-col>
+    </v-row>
+
+    <v-row class="full-height">
+      <v-col cols="2">
+        List
+      </v-col>
+
+      <v-col>
+        Content
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Plugins',
+    props: {
+      flow: {
+        required: true,
+        type: Object
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/view/Flow/SettingsConfigTab.vue
+++ src/view/Flow/SettingsConfigTab.vue
@@ -1,17 +1,27 @@
 <template>
   <v-tabs vertical class="flow-yml-settings full-size">
-    <v-tab>
-      <v-icon left>mdi-account</v-icon>
-      YAML
+    <v-tabs-slider></v-tabs-slider>
+
+    <v-tab href="#tab-yaml">
+      <v-icon left>mdi-code-braces</v-icon>
+      <span class="tab-text">YAML</span>
     </v-tab>
-    <v-tab-item>
+    <v-tab href="#tab-plugins">
+      <v-icon left>mdi-view-grid-plus</v-icon>
+      <span class="tab-text">Plugins</span>
+    </v-tab>
+    <v-tab-item value="tab-yaml">
       <edit-yaml :flow="flow" class="mt-2"></edit-yaml>
+    </v-tab-item>
+    <v-tab-item value="tab-plugins">
+      <plugins :flow="flow" class="mt-2"></plugins>
     </v-tab-item>
   </v-tabs>
 </template>
 
 <script>
   import EditYaml from '@/components/Flow/EditYaml'
+  import Plugins from '@/components/Flow/Plugins'
 
   export default {
     name: 'SettingsConfigTab',
@@ -22,7 +32,8 @@
       }
     },
     components: {
-      EditYaml
+      EditYaml,
+      Plugins
     },
     data () {
       return {
@@ -36,5 +47,8 @@
 </script>
 
 <style scoped>
-
+  .tab-text {
+    min-width: 65px;
+    text-align: left;
+  }
 </style>

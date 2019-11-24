1c4f59ad1510d3ade3614d92da900938cefb56c5
gy2006 <32008001@qq.com>
2019-11-25 00:47:36 +0100

    init summary card component

--- src/view/Flow/Overview.vue
+++ src/view/Flow/Overview.vue
@@ -1,12 +1,30 @@
 <template>
-  <div>
-    overview
-  </div>
+  <v-row align="start" justify="start">
+    <v-col v-for="flow in flows"
+           :key="flow.name"
+           cols="3"
+    >
+      <summary-card ></summary-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+  import SummaryCard from '@/components/Flow/SummaryCard'
+  import { mapState } from 'vuex'
+
   export default {
-    name: 'FlowOverview'
+    name: 'FlowOverview',
+    components: {
+      SummaryCard
+    },
+    computed: {
+      ...mapState({
+        flows: state => state.flows.items,
+        // to receive job updated event and show latest job status on flow list
+        updatedJob: state => state.jobs.updated
+      }),
+    },
   }
 </script>
 
